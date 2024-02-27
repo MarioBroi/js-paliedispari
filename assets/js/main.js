6a7d3c48aee823bb3ce8fa7cc3dcee30ac7deeb8
MarioBroi <inoix16@gmail.com>
2024-02-27 13:24:27 +0000

    add variables and functions to check if the word is a palindrome

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
-console.log('Test');
+//console.log('Test');
+
+//- Palidroma
+
+//    - Chiedere all’utente di inserire una parola 
+//Definiamo la varriabile di input
+const string = prompt('Inserisci una parola: ');
+//console.log(string);
+
+
+//    - Creare una funzione per capire se la parola inserita è palindroma
+function palindromo(str) {
+
+    // ottieni la lunghezza della stringa  
+    const len = string.length;
+
+    // usa un ciclo per dividere a metá la stringa  
+    for (let i = 0; i < len / 2; i++) {
+
+        // controlla che il primo e l'ultimo carattere sono uguali  
+        if (string[i] !== string[len - 1 - i]) {
+            alert(`La parola ${string} non é palindroma`);
+        }
+    }
+    alert(`La parola ${string} é palindroma`);
+}
+
+//Definiamo la varriabile di output
+const value = palindromo(string);
+//console.log(value); 
