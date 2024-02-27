b2d67e94a6428561d38e845dc071b98e02680ba1
MarioBroi <inoix16@gmail.com>
2024-02-27 14:18:30 +0000

    edit random number generator with functions

--- EX_Pari_o_Dispari/assets/js/main.js
+++ EX_Pari_o_Dispari/assets/js/main.js
@@ -4,18 +4,23 @@
 
 //- L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 const pOd = prompt("Scegli tra pari o dispari");
-console.log(pOd);
-
 const userNumber = prompt("inserisci un numero tra 1 e 5");
-console.log(userNumber);
+console.log(`Il giocatore ha scelto ${pOd} ed il numero ${userNumber}`);
 
-//- Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
-const npcNumber = Math.floor(Math.random() * 5) + 1;
-console.log(npcNumber);
+//- Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+/**
+ * Genera un numero random tra 1 e 5
+ * @returns {number}
+ */
+function npcNumberFunction() {
+    const npcNumber = Math.floor(Math.random() * 5) + 1;
+    return npcNumber
+}
+console.log(`il computer ha il numero ${npcNumberFunction()}`);
 
 //- Sommiamo i due numeri 
-const sum = Number(userNumber) + Number(npcNumber);
-console.log(sum);
+const sum = Number(userNumber) + Number(npcNumberFunction());
+console.log(`la somma dei numeri é: ${sum}`);
 
 //- Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
 let numResult;
@@ -25,7 +30,7 @@ if (sum % 2 == 0) {
 } else {
     numResult = "dispari"
 }
-console.log(numResult);
+console.log(`il numero sommato é ${numResult}`);
 
 //- Dichiariamo chi ha vinto.
 if (numResult === pOd) {
